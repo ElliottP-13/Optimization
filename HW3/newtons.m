531af3a17a9bdad8cfc0676824999c6af0c723e7
ElliottP-13 <pryorelliott@gmail.com>
2021-09-30 09:41:37 -0600

    Debugging newtons

--- HW3/newtons.m
+++ HW3/newtons.m
@@ -2,12 +2,12 @@ function x = newtons(f, grad, hessian, x, epsilon)
     k = 0;
     while norm(grad(x)) > epsilon
         k = k + 1;
-        g = grad(x);
+        g = -grad(x);
         F = hessian(x);
         d = F \ g;
-        x = x - d;
+        x = x + d;
         
-        fprintf("k = %d f(x) = %0.5f\t", k, f(x));
+        fprintf("k = %d, f(x) = %0.5f\t", k, f(x));
         fprintf('x : [');
         fprintf('%g, ', x(1:end-1));
         fprintf('%g]\n', x(end));
